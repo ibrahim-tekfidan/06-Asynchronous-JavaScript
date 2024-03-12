522bd42ed1d2b57101a70b8817928993f4e449ba
Ibrahim Tekfidan <ibrahim.tekfidn@gmail.com>
2024-03-12 14:58:13 +0300

    Replacing callbacks with promises

--- index.js
+++ index.js
@@ -1,48 +1,35 @@
-// Callback HELL
-/*
+// In order to resolve this callback hell problem, we should modify our asyncs to return the promise object.
 console.log('before');
-getUser(1, user => {
-  getRepositories(user.gitHubUsername, repos => {
-    getCommits(repos[0], commits => {
-      console.log(commits);
-    });
-  });
-});
-console.log('after');
-*/
-
-// Make async functions look like sync using named functions. First right like above then refactor it. If you don't understand how to convert from callback hell to named function version, you can check out video from Mosh Hamedani Node.js course. It is very simple and understandable.
-console.log('before');
-getUser(1, displayRepos);
+getUser(1)
+  .then(user => getRepositories(user.gitHubUsername))
+  .then(repos => getCommits(repos[0]))
+  .then(commits => console.log(commits))
+  .catch(err => console.log(err.message));
 console.log('after');
 
-function displayRepos(user) {
-  getRepositories(user.gitHubUsername, displayCommits);
-}
-
-function displayCommits(repos) {
-  getCommits(repos[0], commits => {
-    console.log(commits);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Reading a user from database...');
+      resolve({ id: id, gitHubUsername: 'ibrahim' });
+    }, 2000);
   });
 }
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log('Reading a user from database...');
-    callback({ id: id, gitHubUsername: 'ibrahim' });
-  }, 2000);
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`Reading Repositioes from ${username} gitHub account:`);
+      resolve(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+  });
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log(`Reading Repositioes from ${username} gitHub account:`);
-    callback(['repo1', 'repo2', 'repo3']);
-  }, 2000);
-}
-
-function getCommits(repo, callback) {
-  setTimeout(() => {
-    console.log(`Reading commits from ${repo}:`);
-    callback(['commit1', 'commit2', 'commit3']);
-  }, 2000);
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`Reading commits from ${repo}:`);
+      resolve(['commit1', 'commit2', 'commit3']);
+    }, 2000);
+  });
 }
