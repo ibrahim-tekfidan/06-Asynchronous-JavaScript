7240b8e37651e5d8c6b8bf07c4a4dc1f24a25ffb
Ibrahim Tekfidan <ibrahim.tekfidn@gmail.com>
2024-03-12 09:46:04 +0300

    Add another async operation and get object from this async funtion using callback

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ console.log('before');
 // How to use a callback to get the user object
 getUser(1, user => {
   console.log(user);
+  // Get the repos
+  getRepositories(user.gitHubUsername, repos => {
+    console.log(repos);
+  });
 });
 console.log('after');
 
@@ -10,5 +14,12 @@ function getUser(id, callback) {
   setTimeout(() => {
     console.log('Reading a user from database...');
     callback({ id: id, gitHubUsername: 'ibrahim' });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log(`Reading Repositioes from ${username} gitHub account:`);
+    callback(['repo1', 'repo2', 'repo3']);
   }, 2000);
 }
