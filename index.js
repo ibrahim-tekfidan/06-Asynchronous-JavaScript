5f6c3e7898c987057d62dd3076d981a52604b3a0
Ibrahim Tekfidan <ibrahim.tekfidn@gmail.com>
2024-03-12 09:03:44 +0300

    Example of async or non-blocking code.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-// This is an example of sync or blocking program. In this program when the first line of code execute, program is blocking, and second line of code has to wait until the first line finishes execution.
 console.log('before');
+// Because reading a user from database takes some times, we should not block the code. So we need to use async function. setTimout function is not blocking the code. This is simulating async function in javaScript.
+// So when we run this program 'console.log('before')' is executed then we get to the second line. Second line function is scheduling a task to be perform in the future. It doesn't wait or block. Then we g t the third function then third function is executed. And 2 seconds later async setTimout function is executed. So reading a user from database don't block the program.
+setTimeout(() => {
+  console.log('Reading a user from database...');
+}, 2000);
 console.log('after');
