f208dcb6eb992b4247d7cdb98d2b7b88bc3573b2
Ibrahim Tekfidan <ibrahim.tekfidn@gmail.com>
2024-03-12 08:46:03 +0300

    Example of sync or blocking program

---
+++ index.js
@@ -0,0 +1,3 @@
+// This is an example of sync or blocking program. In this program when the first line of code execute, program is blocking, and second line of code has to wait until the first line finishes execution.
+console.log('before');
+console.log('after');
