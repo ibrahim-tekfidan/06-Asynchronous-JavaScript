c824a6c7a1fdf6dabeb179e7f71e403941a71c1b
Ibrahim Tekfidan <ibrahim.tekfidn@gmail.com>
2024-03-12 10:32:07 +0300

    Convert code from Callback hell to named functions

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
+// Callback HELL
+/*
 console.log('before');
-// How to use a callback to get the user object
 getUser(1, user => {
-  console.log(user);
-  // Get the repos
   getRepositories(user.gitHubUsername, repos => {
-    console.log(repos);
+    getCommits(repos[0], commits => {
+      console.log(commits);
+    });
   });
 });
 console.log('after');
+*/
 
-// callback is a function that we are going to call, when result of an async operation is ready
+// Make async functions look like sync using named functions. First right like above then refactor it. If you don't understand how to convert from callback hell to named function version, you can check out video from Mosh Hamedani Node.js course. It is very simple and understandable.
+console.log('before');
+getUser(1, displayRepos);
+console.log('after');
+
+function displayRepos(user) {
+  getRepositories(user.gitHubUsername, displayCommits);
+}
+
+function displayCommits(repos) {
+  getCommits(repos[0], commits => {
+    console.log(commits);
+  });
+}
+
 function getUser(id, callback) {
   setTimeout(() => {
     console.log('Reading a user from database...');
@@ -21,5 +37,12 @@ function getRepositories(username, callback) {
   setTimeout(() => {
     console.log(`Reading Repositioes from ${username} gitHub account:`);
     callback(['repo1', 'repo2', 'repo3']);
+  }, 2000);
+}
+
+function getCommits(repo, callback) {
+  setTimeout(() => {
+    console.log(`Reading commits from ${repo}:`);
+    callback(['commit1', 'commit2', 'commit3']);
   }, 2000);
 }
