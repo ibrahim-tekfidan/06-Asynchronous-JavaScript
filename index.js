2a93f1cb38165251f57960d2c44f420339acbce2
Ibrahim Tekfidan <ibrahim.tekfidn@gmail.com>
2024-03-13 08:08:12 +0300

    Async / Await

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 // In order to resolve this callback hell problem, we should modify our asyncs to return the promise object.
 console.log('before');
-getUser(1)
-  .then(user => getRepositories(user.gitHubUsername))
-  .then(repos => getCommits(repos[0]))
-  .then(commits => console.log(commits))
-  .catch(err => console.log(err.message));
+
+// Promise-based approach
+// getUser(1)
+//   .then(user => getRepositories(user.gitHubUsername))
+//   .then(repos => getCommits(repos[0]))
+//   .then(commits => console.log(commits))
+//   .catch(err => console.log(err.message));
+
+// Async and await approach
+// Async and await helps you write async code like sync code.
+// Any time calling a function that returns a promise you can await the result of that function. And then get the actual result just like calling a sync function.
+// Whenever you use await operator in the function, you need to decorate that function with the async modifier.
+// In our promise base approach we use catch method to get any errors. When using async and await we have this catch method. The way we get the errors is using try-catch block.
+
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+displayCommits();
+
 console.log('after');
 
 function getUser(id) {
@@ -21,6 +43,7 @@ function getRepositories(username) {
     setTimeout(() => {
       console.log(`Reading Repositioes from ${username} gitHub account:`);
       resolve(['repo1', 'repo2', 'repo3']);
+      // reject(new Error('Could not get repositories.'));
     }, 2000);
   });
 }
